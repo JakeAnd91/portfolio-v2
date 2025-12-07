9da28ee7ea728867af747d934b2fd34fa928f3e2
Jacob Anderson <jake.anderson0291@gmail.com>
2025-12-07 05:25:46 -0700

    added imports so vercel could see the images

--- src/pages/Art.jsx
+++ src/pages/Art.jsx
@@ -1,8 +1,12 @@
+import Glitch from "./src/assets/glitch.jpg"
+import NoRisk from "./src/assets/No Risk.jpg"
+import Madden from "./src/assets/Madden.jpg"
+import GreatestLie from "./src/assets/Greatest Lie.jpg"
 const artworks = [
-  "./src/assets/glitch.jpg",
-  "./src/assets/No Risk.jpg",
-  "./src/assets/Madden.jpg",
-  "./src/assets/Greatest Lie.jpg"
+Glitch,
+NoRisk,
+Madden,
+GreatestLie
 ]
 
 export default function Art() {
